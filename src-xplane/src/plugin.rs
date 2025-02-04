60bb3810c980c11184a27c6687ba0963fe4636f0
Zodiac <di.zou1214@gmail.com>
2025-02-03 21:15:01 -0500

    feat: turn on/off backlights when XP opens/closes

--- src-xplane/src/plugin.rs
+++ src-xplane/src/plugin.rs
@@ -1,6 +1,9 @@
 use crate::flight_loop::FlightLoopHandler;
 use crate::plugin_debugln;
 use crate::vibration::start_vibration_thread;
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
 use xa_ursa_minor_hid::hid::HIDWrapper;
 use xplm::data::borrowed::DataRef;
 use xplm::flight_loop::FlightLoop;
@@ -8,6 +11,7 @@ use xplm::plugin::{Plugin, PluginInfo};
 
 pub struct UrsaMinorPlugin {
     flight_loop: FlightLoop,
+    hidwrapper: Arc<Mutex<HIDWrapper>>,
 }
 
 impl Plugin for UrsaMinorPlugin {
@@ -19,6 +23,7 @@ impl Plugin for UrsaMinorPlugin {
 
         let (tx, r_) = std::sync::mpsc::channel();
         let plugin = Self {
+            hidwrapper: Arc::new(Mutex::new(HIDWrapper::new().unwrap())),
             flight_loop: FlightLoop::new(FlightLoopHandler {
                 g_force_y: DataRef::find("sim/flightmodel2/misc/gforce_axil").unwrap(),
                 g_force_x: DataRef::find("sim/flightmodel2/misc/gforce_side").unwrap(),
@@ -47,6 +52,18 @@ impl Plugin for UrsaMinorPlugin {
             last_g_force_z: 0.0,
             tx: tx,
         });
+        // Clone the Arc pointer (cheap operation) for sharing with the spawned thread.
+        let hidwrapper_shared = Arc::clone(&self.hidwrapper);
+        thread::spawn(move || {
+            for i in 0..255 {
+                {
+                    // Lock the mutex to safely use the HIDWrapper.
+                    let mut hw = hidwrapper_shared.lock().unwrap();
+                    hw.write_backlight(i).unwrap();
+                }
+                thread::sleep(Duration::from_millis(5));
+            }
+        });
         self.flight_loop.schedule_immediate();
         Ok(())
     }
@@ -54,6 +71,16 @@ impl Plugin for UrsaMinorPlugin {
     fn disable(&mut self) {
         self.flight_loop.deactivate();
         HIDWrapper::new().unwrap().write_vibration(0).unwrap();
+        // Clone the Arc pointer (cheap operation) for sharing with the spawned thread.
+        let hidwrapper_shared = Arc::clone(&self.hidwrapper);
+        for i in 0..255 {
+            {
+                // Lock the mutex to safely use the HIDWrapper.
+                let mut hw = hidwrapper_shared.lock().unwrap();
+                hw.write_backlight(255 - i).unwrap();
+                thread::sleep(Duration::from_millis(5));
+            }
+        }
     }
 
     fn info(&self) -> PluginInfo {
